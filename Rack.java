52a43001805fead64ebdd18124755d6eba30b847
Suvin <45140795+suvinseal@users.noreply.github.com>
2018-12-20 21:54:35 -0500

    Add files via upload

---
+++ Rack.java
@@ -0,0 +1,102 @@
+
+package finalProject;
+
+/**
+ *
+ * @author Suvin
+ */
+public class Rack {
+    
+    private String UPSSerialNum;
+    private String rackRoomNum;
+    private String rackNum;
+    private String make;
+    private String model;
+    private String MFGDate;
+    private String UPSType;
+    private String numOfSwitchesPluggedIn;
+    private String surveyID;
+
+    public String getUPSSerialNum() {
+        return UPSSerialNum;
+    }
+
+    public void setUPSSerialNum(String UPSSerialNum) {
+        this.UPSSerialNum = UPSSerialNum;
+    }
+
+    public String getRackRoomNum() {
+        return rackRoomNum;
+    }
+
+    public void setRackRoomNum(String rackRoomNum) {
+        this.rackRoomNum = rackRoomNum;
+    }
+
+    public String getRackNum() {
+        return rackNum;
+    }
+
+    public void setRackNum(String rackNum) {
+        this.rackNum = rackNum;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMFGDate() {
+        return MFGDate;
+    }
+
+    public void setMFGDate(String MFGDate) {
+        this.MFGDate = MFGDate;
+    }
+
+    public String getUPSType() {
+        return UPSType;
+    }
+
+    public void setUPSType(String UPSType) {
+        this.UPSType = UPSType;
+    }
+
+    public String getNumOfSwitchesPluggedIn() {
+        return numOfSwitchesPluggedIn;
+    }
+
+    public void setNumOfSwitchesPluggedIn(String numOfSwitchesPluggedIn) {
+        this.numOfSwitchesPluggedIn = numOfSwitchesPluggedIn;
+    }
+
+    public String getSurveyID() {
+        return surveyID;
+    }
+
+    public void setSurveyID(String surveyID) {
+        this.surveyID = surveyID;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
