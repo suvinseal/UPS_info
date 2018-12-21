191314a9b125c90071d98684a14b044e852c3958
Suvin <45140795+suvinseal@users.noreply.github.com>
2018-12-20 21:55:11 -0500

    Add files via upload

---
+++ SurveyInfo.java
@@ -0,0 +1,29 @@
+
+package finalProject;
+
+/**
+ *
+ * @author Suvin 
+ */
+public class SurveyInfo {
+    
+    private String surveyID;
+    private String buildingID;
+
+    public String getSurveyID() {
+        return surveyID;
+    }
+
+    public void setSurveyID(String surveyID) {
+        this.surveyID = surveyID;
+    }
+
+    public String getBuildingID() {
+        return buildingID;
+    }
+
+    public void setBuildingID(String buildingID) {
+        this.buildingID = buildingID;
+    }
+    
+}
