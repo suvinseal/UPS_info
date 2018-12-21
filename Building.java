54b0c16acbbf71c335f57a00881f2757a410b654
Suvin <45140795+suvinseal@users.noreply.github.com>
2018-12-20 21:54:56 -0500

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,29 @@
+
+package finalProject;
+
+/**
+ *
+ * @author Suvin
+ */
+public class Building {
+    
+    private String buildingID;
+    private String buildingName;
+
+    public String getBuildingID() {
+        return buildingID;
+    }
+
+    public void setBuildingID(String buildingID) {
+        this.buildingID = buildingID;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+    
+}
